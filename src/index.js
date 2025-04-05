011eaa587394f57b3c9d31717aacb868dd7084d5
asithandilel <97704270+AsithandileL@users.noreply.github.com>
2025-04-05 18:34:55 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+document.getElementById('getJobs').addEventListener('click',getJobs)
+async function getJobs() {
+    let url = 'https://jobicy.com/api/v2/remote-jobs';
+     try {
+        let response = await fetch(url);
+
+        if(!response.ok){
+            throw new Error("Something went wrong");
+        }
+        else{
+            let jobs = await response.json();
+            //console.log(jobs.jobs[0].companyName);
+            document.getElementById('display').innerHTML = `Company Name: ${jobs.jobs[0].companyName}`;
+            
+        }
+     } catch (error) {
+        console.warn(error.message);
+     }
+    
+}
